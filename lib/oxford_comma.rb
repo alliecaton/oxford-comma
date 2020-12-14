b6c2e3734427632f32f3db3e8342c0f217cfbf55
Allie <alliecaton3@gmail.com>
2020-12-14 17:25:36 -0500

    commit tree c9637c2a263603f5fe99d11e866a4c70367af37d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    last_element = "and " + array.last
+    if array.length == 2
+        return array.join (" and ")
+    elsif array.length > 2
+        array.pop
+        array.insert(-1, last_element)
+    end
+    array.join(", ")
 end
